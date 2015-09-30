28cfd4a4e037a078ddb9715495d2b2511abecbf8
Ryan Walker <ryan@maddenwalker.com>
2015-09-29 21:20:27 -0700

    Assignment 19 Storyboard, Completed

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Ryan Walker on 9/29/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Ryan Walker on 9/29/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
