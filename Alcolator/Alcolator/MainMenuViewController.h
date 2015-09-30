7d6ecb6af15c27b70f9509256782dfb690b96236
Ryan Walker <ryan@maddenwalker.com>
2015-09-30 13:32:40 -0700

    Checkpoint 21 Basic Navigation, Completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Ryan Walker on 9/30/15.
+//  Copyright © 2015 Ryan Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
